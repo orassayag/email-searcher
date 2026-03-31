5ff9d63013657f9613965633c48c7291d0342fd5
Or Assayag <orassayag@gmail.com>
2026-03-31 19:19:56 +0300

    Restore missing files

---
+++ project-template/garo-estate-master_before_registration_&_404/assets/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $('.layout-grid').on('click', function() {
+        $('.layout-grid').addClass('active');
+        $('.layout-list').removeClass('active');
+
+        $('#list-type').removeClass('proerty-th-list');
+        $('#list-type').addClass('proerty-th');
+
+    });
+
+    $('.layout-list').on('click', function() {
+        $('.layout-grid').removeClass('active');
+        $('.layout-list').addClass('active');
+
+        $('#list-type').addClass('proerty-th-list');
+        $('#list-type').removeClass('proerty-th');
+
+    });
+
+});
+$(document).ready(function() {
+    var RGBChange = function() {
+        $('#RGB').css('background', '#FDC600')
+    };
+
+    function sdf_FTS(_number, _decimal, _separator) {
+        var decimal = (typeof(_decimal) != 'undefined') ? _decimal : 2;
+        var separator = (typeof(_separator) != 'undefined') ? _separator : '';
+        var r = parseFloat(_number)
+        var exp10 = Math.pow(10, decimal);
+        r = Math.round(r * exp10) / exp10;
+        rr = Number(r).toFixed(decimal).toString().split('.');
+        b = rr[0].replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g, "\$1" + separator);
+        r = (rr[1] ? b + '.' + rr[1] : b);
+
+        return r;
+    }
+
+})
+
+// Initializing WOW.JS
+
+new WOW().init();
